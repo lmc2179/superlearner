2a9a077e4906ebb5d1f1749e959b9ec1d23f7b46
lmc2179 <louiscialdella@gmail.com>
2018-12-06 23:12:48 -0600

    fixed some bugs

--- superlearner/model.py
+++ superlearner/model.py
@@ -3,8 +3,9 @@ from sklearn.model_selection import cross_val_predict, KFold
 import numpy as np
 from copy import deepcopy
 from sklearn.linear_model import LinearRegression
+from sklearn.base import BaseEstimator
 
-class SuperLearnerRegressor(object):
+class SuperLearnerRegressor(BaseEstimator):
     def __init__(self, base_learners, k_folds=3):
         self.base_learners = base_learners
         self.meta_learner = LinearRegression()
@@ -14,11 +15,11 @@ class SuperLearnerRegressor(object):
 
     def fit(self, X, y):
         seed = np.random.randint(-1e6, 1e6)
-        y_pred_learners = [cross_val_predict(X, y, l, KFold(self.k_folds, random_state=seed)) for l in self.base_learners]
-        self.trained_learners = [deepcopy(l).fit(X, y) for l in self.base_learners]
+        y_pred_learners = [cross_val_predict(l, X, y, cv=KFold(self.k_folds, random_state=seed)) for l in self.base_learners]
+        self.trained_learners_ = [deepcopy(l).fit(X, y) for l in self.base_learners]
         Z = np.vstack(y_pred_learners).T
         self.trained_meta_learner_ = deepcopy(self.meta_learner)
-        self.trained_meta_learner.fit(Z, y)
+        self.trained_meta_learner_.fit(Z, y)
         
     def predict(self, X):
         y_pred_learners = [l.predict(X) for l in self.trained_learners_]
